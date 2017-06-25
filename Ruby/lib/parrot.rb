65fdec22239edc952315528e03168099283b923f
Katerina <katerinaldg@gmail.com>
2017-06-25 12:44:46 +0100

    Refactored kata

--- Ruby/lib/parrot.rb
+++ Ruby/lib/parrot.rb
@@ -1,4 +1,6 @@
 class Parrot
+  BASE_SPEED = 12.0
+
   def initialize type, number_of_coconuts, voltage, nailed
     @type = type;
     @number_of_coconuts = number_of_coconuts;
@@ -7,47 +9,45 @@ class Parrot
   end
 
   def speed
-    case @type
-    when :european_parrot
-      return EuropeanParrot.new.speed
-    when :african_parrot
-      return AfricanParrot.new(@number_of_coconuts).speed
-    when :norwegian_blue_parrot
-      return (@nailed) ? 0 : compute_base_speed_for_voltage(@voltage);
-    end
+    parrots_speed = {
+      european_parrot: BASE_SPEED,
+      african_parrot: AfricanParrot.new(BASE_SPEED, @number_of_coconuts).speed,
+      norwegian_blue_parrot: NorwegianBlueParrot.new(BASE_SPEED, @nailed, @voltage).speed
+    }
+    parrots_speed[@type]
+  end
 
-    throw "Should be unreachable!";
+end
+
+class AfricanParrot
+  LOAD_FACTOR = 9.0
+
+  def initialize(base_speed, number_of_coconuts)
+    @base_speed = base_speed
+    @number_of_coconuts = number_of_coconuts
+  end
+
+  def speed
+    [0, @base_speed - LOAD_FACTOR * @number_of_coconuts].max
+  end
+end
+
+class NorwegianBlueParrot
+
+  def initialize(base_speed, nailed, voltage)
+    @base_speed = base_speed
+    @nailed = nailed
+    @voltage = voltage
+  end
+
+  def speed
+    (@nailed) ? 0 : compute_base_speed_for_voltage
   end
 
   private
 
-  def compute_base_speed_for_voltage voltage
-   [24.0, voltage * base_speed].min
+  def compute_base_speed_for_voltage
+    [24.0, @voltage * @base_speed].min
   end
 
-  def load_factor
-    9.0;
-  end
-
-  def base_speed
-    12.0;
-  end
-end
-
-class EuropeanParrot
-  def speed
-    12.0
-  end
-end
-
-class AfricanParrot
-  BASE_SPEED = 12.0
-  LOAD_FACTOR = 9.0
-
-  def initialize(number_of_coconuts)
-    @number_of_coconuts = number_of_coconuts
-  end
-  def speed
-    [0, BASE_SPEED - LOAD_FACTOR * @number_of_coconuts].max
-  end
 end
